60e96a8fdb2e0a40b33e9501a9300e7c7cc68614
Tulasisaipavan <98252438+Tulasisaipavan@users.noreply.github.com>
2022-02-07 17:44:38 +0530

    Methods code

---
+++ Methods.java
@@ -0,0 +1,42 @@
+package com.assignments;
+
+public class Methods {
+		public void hello()
+		{
+			System.out.println("Inside hello method. No return type");
+		}
+		
+		public int add(int a, int b)
+		{
+			return a+b;
+		}
+		
+		public double mul(double a, double b)
+		{
+			return a*b;
+		}
+		
+		public float div(float a, float b)
+		{
+			return a/b;
+		}
+		
+		public String ending()
+		{
+			return "end program";
+		}
+
+		public static void main(String[] args) {
+			Methods m = new Methods();
+			m.hello();
+			System.out.println(m.add(20, 30));
+			System.out.println(m.mul(50, 80));
+			System.out.println(m.div(200, 10));
+			System.out.println(m.ending());
+
+		}
+
+
+	}
+
+
